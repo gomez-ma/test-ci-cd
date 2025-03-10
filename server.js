0bda247b8e1377de599b2faf29cf2127e453c3a4
gomez-ma <komez@live.com>
2025-03-10 11:07:59 +0700

    test ci/cd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT =  5000;
 
 app.get("/", (req, res) => {
-    res.send("CI/CD");
+    res.send("testCI/CD");
 })
 
 app.listen(PORT, () => console.log('Server is running ' + PORT));
